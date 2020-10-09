9c62d9d7b465fe764db0a0a56f8694c20296e1a9
rajamingirsang <sinagabaya@gmail.com>
2020-10-09 14:53:32 +0700

    buat file Tugas1SirJimmy.php

---
+++ Tugas1SirJimmy.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+     <html>
+      <head>
+      	<title> Riwayat Hidup</title>
+   </head>
+
+    <body>
+     <table border = "3" style = background-color:aqua>
+     	 <tr>
+    		  <td style = background-color:blue colspan = "3"><center><b><font color = white size = 30 >Riwayat Hidup</font></b></center>  </td>
+	  </tr>
+    <tr>
+	 <td style = background-color:blue colspan = "3"><center><b><font color = white size = 30 > Biodata
+	</font></b></center></td>
+  </tr>
+
+ <tr>
+	  <td>Nama</td>
+	  <td>Rajamin Rajaoloan Girsang</td>
+
+  </tr>
+
+  <tr>  
+  <td>Nama Kampus</td>
+	  <td>Universitas IBBI</td>
+   </tr>
+
+  <tr>
+	<td>Nim</td>
+    <td>1715121620 </td>
+ </tr>
+
+ <tr>
+	<td>Program</td>
+	<td>S1</td>
+  </tr>
+
+<tr>
+	<td>Jurusan</td>
+	<td>Teknik Informatika </td>
+ </tr>
+
+<tr>
+	<td>Kelas</td>
+	<td>T1-P1</td>
+  </tr>
+
+ <tr>
+	<td>Dosen Wali</td>
+	<td>Conrad Bombongan</td>
+  </tr>
+
+ <tr>
+	<td>Tangga Lahir </td>
+	<td>26 Juni1998</td>
+  </tr>
+
+ <tr>
+	<td>Jenis Kelamin</td>
+	<td>Laki-Laki</td>
+</tr>
+
+  <tr>
+	<td>Warga </td>
+	<td>WNI</td>
+  </tr>
+<tr>
+
+	<td>Agama</td>
+	<td>Kristen Protestan</td>
+ </tr>
+
+ <tr>
+	<td>Golongan Darah</td>
+	<td>A</td>
+  </tr>
+
+<tr>
+	<td>Alamat</td>
+	<td>Simalungun,Sumatera Utara, Indonesia</td>
+  </tr>
+
+ </table>
+ </body>
+  </html>
